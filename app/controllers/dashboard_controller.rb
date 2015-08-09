1b301448ac83e9cecea2c978f470ae34fe236447
Sally MacNicholas <smacnicholas@gmail.com>
2015-08-08 19:12:16 -0600

    keys

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def index
     @client = FitbitService.new(current_user)
+    # binding.pry
     @steps_needed_to_hit_goal = current_user.goal.steps_needed
   end
 end
